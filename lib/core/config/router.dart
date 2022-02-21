56ec417dbf68e5607125ac5885132df6883873e0
Anh Nguyen <anh@neon.dev>
2022-02-21 17:30:23 +0100

    add setting, project pages

--- lib/core/config/router.dart
+++ lib/core/config/router.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:glance/core/presentation/pages/home_page.dart';
+import 'package:glance/features/create/presentation/pages/create_page.dart';
+import 'package:glance/features/settings/presentation/pages/setting_page.dart';
 import 'package:go_router/go_router.dart';
 
 const kRouteAppLoader = 'app_loader';
@@ -7,31 +12,69 @@ const kRouteHome = 'home';
 const kRouteLogin = 'login';
 const kRouteConfirmation = 'confirmation';
 const kRouteCreate = 'create';
+const kRouteSetting = 'setting';
 
 class AppRouter {
-  AppRouter(this.appContext);
-
-  final BuildContext appContext;
-
   // final _projectBloc = getIt<ProjectBloc>();
+
+  Page<void> getPlatformApp({
+    LocalKey? key,
+    bool? fullscreen,
+    required Widget child,
+  }) {
+    return Platform.isIOS
+        ? CupertinoPage(
+            key: key,
+            child: child,
+            fullscreenDialog: fullscreen ?? false,
+          )
+        : MaterialPage(
+            key: key,
+            child: child,
+            fullscreenDialog: fullscreen ?? false,
+          );
+  }
 
   late final router = GoRouter(
     routes: [
       GoRoute(
-        name: kRouteAppLoader,
+        name: kRouteHome,
         path: '/',
         builder: (context, state) => const HomePage(),
       ),
-      // GoRoute(
-      //   name: kRouteConfirmation,
-      //   path: '/confirmation',
-      //   builder: (context, state) => const ConfirmationPage(),
-      // ),
-      // GoRoute(
-      //   name: kRouteLogin,
-      //   path: '/login',
-      //   builder: (context, state) => const LoginPage(),
-      // ),
+      GoRoute(
+        name: kRouteSetting,
+        path: '/setting',
+        pageBuilder: (context, state) => getPlatformApp(
+          key: state.pageKey,
+          fullscreen: true,
+          child: const SettingPage(),
+        ),
+      ),
+      GoRoute(
+        name: kRouteCreate,
+        path: '/create',
+        pageBuilder: (context, state) => getPlatformApp(
+          key: state.pageKey,
+          fullscreen: true,
+          child: const CreatePage(),
+        ),
+
+        /* CustomTransitionPage<void>(
+          key: state.pageKey,
+          child: const CreatePage(),
+          fullscreenDialog: true,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) => SlideTransition(
+            position: animation.drive(
+              Tween<Offset>(
+                begin: const Offset(0, 0.8),
+                end: Offset.zero,
+              ).chain(CurveTween(curve: Curves.easeInOut)),
+            ),
+            child: child,
+          ),
+        ), */
+      ),
     ],
     redirect: (state) {
       // final _authState = appContext.read<AuthBloc>().state;
--- lib/features/settings/presentation/pages/setting_page.dart
+++ lib/features/settings/presentation/pages/setting_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class ProjectPage extends StatelessWidget {
-  const ProjectPage({Key? key}) : super(key: key);
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.teal,
+      color: Colors.blue,
       child: null,
     );
   }
