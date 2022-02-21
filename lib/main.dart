2d52d97f26f6c812d53995663e5f39396c6286e2
Anh Nguyen <anh@neon.dev>
2022-02-21 17:29:18 +0100

    add loggy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:loggy/loggy.dart';
 
 import 'core/presentation/pages/app.dart';
 
 void main() {
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(),
+  );
   runApp(const MyApp());
 }
