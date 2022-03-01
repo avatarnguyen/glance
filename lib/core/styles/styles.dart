9f14fe76204bc6ed1ad47f115234830a4667e6b8
Anh Nguyen <anh@neon.dev>
2022-03-01 23:12:40 +0100

    textbutton widget with cupertinobutton

--- lib/core/styles/styles.dart
+++ lib/core/styles/styles.dart
@@ -5,3 +5,4 @@ export 'styling_constants.dart';
 export 'widgets/buttons/outlined_button.dart';
 export 'widgets/buttons/filled_button.dart';
 export 'widgets/buttons/icon_button.dart';
+export 'widgets/buttons/text_button.dart';
---
+++ lib/core/styles/widgets/buttons/text_button.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:glance/core/glance_core.dart';
+import 'package:glance/core/styles/widgets/buttons/base_button.dart';
+
+class AppTextButton extends StatelessWidget {
+  const AppTextButton({
+    Key? key,
+    required this.onPressed,
+    required this.data,
+    required this.size,
+    this.color,
+    this.padding,
+  }) : super(key: key);
+
+  const AppTextButton.small({
+    Key? key,
+    required this.onPressed,
+    required this.data,
+    this.color,
+    this.padding,
+  })  : size = ButtonSize.small,
+        super(key: key);
+
+  const AppTextButton.regular({
+    Key? key,
+    required this.onPressed,
+    required this.data,
+    this.color,
+    this.padding,
+  })  : size = ButtonSize.regular,
+        super(key: key);
+
+  const AppTextButton.big({
+    Key? key,
+    required this.onPressed,
+    required this.data,
+    this.color,
+    this.padding,
+  })  : size = ButtonSize.big,
+        super(key: key);
+
+  final void Function() onPressed;
+  final String data;
+  final Color? color;
+  final ButtonSize size;
+  final EdgeInsets? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = AppTheme.of(context);
+    final _color = color ?? theme.colors.primary1;
+    final textstyle = () {
+      switch (size) {
+        case ButtonSize.small:
+          return theme.typography.paragraph1;
+        case ButtonSize.regular:
+          return theme.typography.title4;
+        case ButtonSize.big:
+          return theme.typography.title3;
+        default:
+          return theme.typography.paragraph1;
+      }
+    }();
+
+    return CupertinoButton(
+      padding: padding ?? EdgeInsets.zero,
+      child: Text(
+        data,
+        style: textstyle.copyWith(color: _color),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
