9ce75b51ee59a01464c42af042ed9cd1f00ce8fe
anhNEON <anh@neon.dev>
2022-05-03 20:48:03 +0200

    add: packages for handling timzone

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'core/presentation/pages/app.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() {
+  tz.initializeTimeZones();
+
   runApp(
     const ProviderScope(
       child: MyApp(),
