70b6c80906e9e639c11d3b6e54ca65f7d5df1c4d
anhNEON <anh@neon.dev>
2022-05-04 12:06:37 +0200

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'firebase_options.dart';
 
 import 'core/presentation/pages/app.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   tz.initializeTimeZones();
 
   await dotenv.load();
